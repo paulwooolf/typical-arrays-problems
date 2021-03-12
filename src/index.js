9e7be415756634913d518894c7a3afb1c496eb71
paulwooolf <paulwooolf@gmail.com>
2021-03-13 00:31:45 +0300

    feature: write min, max and avg func

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    if (typeof array !== 'undefined' && array.length > 0) {
+        let min = array[0];
+        array.forEach(item => {
+            if (item < min) {
+                min = item;
+            }
+        })
+        return min;
+    }
+    return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (typeof array !== 'undefined' && array.length > 0) {
+        let max = array[0];
+        array.forEach(item => {
+            if (item > max) {
+                max = item;
+            }
+        })
+        return max;
+    }
+    return 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (typeof array !== 'undefined' && array.length > 0) {
+        return array.reduce((acc, item) => acc += item) / array.length;
+    }
+    return 0;
 }
